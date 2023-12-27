8894a5321286d2cde8c4fe48ba9c019dbf43f38a
Rashid <rash.sh.77@gmail.com>
2023-12-27 20:12:41 +0500

    fixed bugs in app chart, auth chart started

--- backend/api/api_v1/api.py
+++ backend/api/api_v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from api.api_v1.endpoints import users
+from api.api_v1.endpoints import users, ping
 
 api_router = APIRouter()
+api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
 api_router.include_router(users.router, prefix="/user", tags=["user"])
