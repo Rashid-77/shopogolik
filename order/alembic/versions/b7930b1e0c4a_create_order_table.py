5a09c455696080ad32d31df0b46082d326186f86
Rashid <rash.sh.77@gmail.com>
2024-01-24 20:15:39 +0500

    uuid field added to order for idempotency, create table for order and user in order service added

---
+++ order/alembic/versions/b7930b1e0c4a_create_order_table.py
@@ -0,0 +1,49 @@
+"""create order table
+
+Revision ID: b7930b1e0c4a
+Revises: acdd1da395cb
+Create Date: 2024-01-04 15:58:22.905105
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'b7930b1e0c4a'
+down_revision: Union[str, None] = 'acdd1da395cb'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "order",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("uuid", sa.Uuid(), nullable=False),
+        sa.Column("userId", sa.Integer(), nullable=False),
+        sa.Column("amount", sa.Float(), nullable=True),
+        sa.Column("shipName", sa.String(), nullable=True),
+        sa.Column("shipAddr", sa.String(), nullable=True),
+        sa.Column("city", sa.String(), nullable=True),
+        sa.Column("state", sa.String(), nullable=True),
+        sa.Column("zip", sa.String(), nullable=True),
+        sa.Column("country", sa.String(), nullable=True),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("phone", sa.String(), nullable=False),
+        sa.Column("tax", sa.Float(), nullable=True),
+        sa.Column("shiped", sa.Boolean(), nullable=True),
+        sa.Column("shipDate", sa.DateTime(), nullable=True),
+        sa.Column("trackinNumber", sa.String(), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_order_id"), "order", ["id"], unique=True)
+    op.create_index(op.f("ix_order_uuid"), "order", ["uuid"], unique=True)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_order_uuid"), table_name="order")
+    op.drop_index(op.f("ix_order_id"), table_name="order")
+    op.drop_table("order")
--- order/schemas/__init__.py
+++ order/schemas/__init__.py
@@ -1,2 +1,3 @@
 from .token import Token
 from .order import Order, OrderCreate, OrderInDBBase, OrderUpdate
+from .user import UserCreate
