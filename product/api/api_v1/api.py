6308384b2485c27ee3abe607da36f574407fd9ba
Rashid <rash.sh.77@gmail.com>
2024-02-15 19:33:11 +0500

    now unauthorized users can view amount of certain good in stock

--- product/api/api_v1/api.py
+++ product/api/api_v1/api.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from api.api_v1.endpoints import ping, product, stock
+from api.api_v1.endpoints import ping, product, stock, stock_info
 
 api_router = APIRouter()
-api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
+api_router.include_router(ping.router, prefix="/product/ping", tags=["ping"])
 api_router.include_router(product.router, prefix="/product", tags=["product"])
 api_router.include_router(stock.router, prefix="/stock/product", tags=["stock"])
-
+api_router.include_router(stock_info.router, prefix="/stock/prod-info", tags=["prod-info"])
